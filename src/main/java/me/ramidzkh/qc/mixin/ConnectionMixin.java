6e1b18139add0dfa77f6b05c68a18e4370e28439
Ramid Khan <ramidzkh@gmail.com>
2023-08-18 18:11:34 +1000

    Trigger full disconnect client disconnects

--- src/main/java/me/ramidzkh/qc/mixin/ConnectionMixin.java
+++ src/main/java/me/ramidzkh/qc/mixin/ConnectionMixin.java
@@ -1,6 +1,8 @@
 package me.ramidzkh.qc.mixin;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.incubator.codec.quic.QuicStreamChannel;
 import me.ramidzkh.qc.client.QuicConnection;
 import me.ramidzkh.qc.client.QuicSocketAddress;
 import me.ramidzkh.qc.client.QuicTier;
@@ -9,6 +11,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -17,6 +20,9 @@ import java.util.concurrent.ExecutionException;
 
 @Mixin(Connection.class)
 public class ConnectionMixin {
+
+    @Shadow
+    private Channel channel;
 
     @Shadow
     private boolean encrypted;
@@ -30,6 +36,15 @@ public class ConnectionMixin {
             if (quicAddress.getQuicTier() == QuicTier.QUIC_ONLY) {
                 callbackInfoReturnable.setReturnValue(QuicConnection.connect(address, useNativeTransport, connection));
             }
+        }
+    }
+
+    @Redirect(method = "disconnect", at = @At(value = "FIELD", target = "Lnet/minecraft/network/Connection;channel:Lio/netty/channel/Channel;"))
+    private Channel getChannelForDisconnect(Connection self) {
+        if (channel instanceof QuicStreamChannel quic) {
+            return quic.parent();
+        } else {
+            return channel;
         }
     }
 
